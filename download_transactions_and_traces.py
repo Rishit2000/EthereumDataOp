bf28e503b9d191bb2d4db1f08b338f926e0233bc
Rishit2000 <42579830+Rishit2000@users.noreply.github.com>
2025-06-24 23:56:26 +0530

    Add files via upload

---
+++ download_transactions_and_traces.py
@@ -0,0 +1,24 @@
+from google.cloud import bigquery
+import pandas as pd
+
+client = bigquery.Client()
+
+query = """
+SELECT *
+FROM `bigquery-public-data.crypto_ethereum_classic.transactions`
+WHERE block_number <= 100000
+"""
+
+df = client.query(query).to_dataframe()
+df.to_csv("transactions.csv", index=False)
+print("Saved data to transactions.csv")
+
+query = """
+SELECT *
+FROM `bigquery-public-data.crypto_ethereum_classic.traces`
+WHERE block_number <= 100000
+"""
+
+df = client.query(query).to_dataframe()
+df.to_csv("traces.csv", index=False)
+print("Saved data to traces.csv")
